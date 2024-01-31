28cb23c1f233ab3a2ab4c2d0a3bd00666d64f6f1
Yve Volkovaya <yvevolkovaya@gmail.com>
2024-01-31 20:03:14 +0000

    start creating profile

--- crowpilotReactN/Screens/Profile.js
+++ crowpilotReactN/Screens/Profile.js
@@ -1,13 +1,44 @@
-import { useContext } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
+import { useState, useEffect } from 'react'
+import { getUser, getUserPhotos } from "../api";
+import moment from 'moment';
 import { AuthContext } from '../Contexts/AuthContext';
 
 export default function Profile() {
-    const { setUserToken } = useContext(AuthContext);
+const [user, setUser] = useState({})
+const [userPhotos, setUserPhotos] = useState([])
+//const { setUserToken } = useContext(AuthContext);
+
+
+//user is currently hard coded in
+useEffect(() => {
+    getUser("hharr").then((user) => {
+        setUser(user);
+        getUserPhotos(user.username).then((photos) => {
+            setUserPhotos(photos)
+        })
+    })
+}, [])
+
     return (
         <View>
-            <Text>This is Profile</Text>
-            <Button title="log out" onPress={() => setUserToken(null)}/>
+            <Text style = {styles.name}>{user.firstname} {user.surname}</Text>
+            <Text style = {styles.subtitle}>{user.username}</Text>
+            <Image style = {{"height": 100, "width": 100}} source={{uri: `${user.avatar_url}`}}></Image>
+            <Text>Member since: {moment(user.acc_created).format('MMM yyyy')}</Text>
+            <Text>Photos taken: {userPhotos.length}</Text>
+            <View></View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    name: {
+        textAlign: 'center',
+        fontSize: 40
+    },
+    subtitle: {
+        fontSize: 30,
+        textAlign: 'center'
+    }
+})
--- crowpilotReactN/api.js
+++ crowpilotReactN/api.js
@@ -12,4 +12,22 @@ export const getAllPhotos = async () => {
       console.log(err);
     }
   };
+
+export const getUser = async (username) => {
+  try {
+    const response = await request.get(`/users/${username}`)
+    return response.data;
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+export const getUserPhotos = async (username) => {
+  try {
+    const response = await request.get(`/photos/${username}`)
+    return response.data;
+  } catch (err) {
+    console.log(err)
+  }
+}
   
