e619f7873443e9337806aa795411961e91d266f4
Jakub Wiszowaty <jakubwiszowaty@hotmail.com>
2024-02-23 14:40:36 +0100

    coordinates of intermediate point

--- Coordinates/Haversine.js
+++ Coordinates/Haversine.js
@@ -5,10 +5,17 @@ function haversineDistance(coords1, coords2) {
     }
 
     const lat1 = coords1[0];
+    const phi1 = toRad(lat1)
+
     const lon1 = coords1[1];
+    const lambda1 = toRad(lon1)
+
 
     const lat2 = coords2[0];
+    const phi2 = toRad(lat2)
+
     const lon2 = coords2[1];
+    const lambda2 = toRad(lon2)
 
     const R = 6371; // km
 
@@ -22,14 +29,18 @@ function haversineDistance(coords1, coords2) {
 
     // if(isMiles) d /= 1.60934;
     console.log(d, 'km')
+    const fraction = 0.1 // must be between 0-1
+    const a2 = Math.sin((1-fraction)*c)/Math.sin(c)
+    const b = Math.sin(fraction*c)/Math.sin(c)
+    const x = a2 * Math.cos(phi1) * Math.cos(lambda1) + b * Math.cos(phi2) * Math.cos(lambda2) 
+    const y = a2 * Math.cos(phi1) * Math.sin(lambda1) + b * Math.cos(phi2) * Math.sin(lambda2) 
+    const z = a2 * Math.sin(phi1) + b * Math.sin(phi2) 
+    const phii = Math.atan2(z, Math.sqrt(Math.pow(x,2) + Math.pow(y,2)))
+    const lambdai = Math.atan2(y, x)
+    const lati = phii / (Math.PI / 180)
+    const loni = lambdai / (Math.PI / 180)
     
-    const a =
-    const b =
-    const x =
-    const y =
-    const z = 
-    const lati =
-    const loni
+    console.log([lati, loni]) //intermediate point coordinates
 }
 //https://www.movable-type.co.uk/scripts/latlong.html
 haversineDistance(EGLL,LFPG)
