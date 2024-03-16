7ebb35452f93fd85bab2d78c6e6cd89f4cec0fad
Yve Volkovaya <yvevolkovaya@gmail.com>
2024-03-16 19:45:57 +0000

    set up project with no ui library

--- crowpilotReactN/App.js
+++ crowpilotReactN/App.js
@@ -1,8 +1,6 @@
 import 'react-native-gesture-handler';
 import AuthenticationFlows from './Screens/AuthenticationFlows';
 import { AuthProvider } from './Contexts/AuthContext';
-import { Cloudinary } from "@cloudinary/url-gen";
-
 
 export default function App() {
     
